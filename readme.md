ab71cf0d1c728036edf62d1e62246862ff389902
KepoDes <kepodev3@gmail.com>
2026-06-11 22:29:06 +0700

    UP

---
+++ readme.md
@@ -0,0 +1,6 @@
+# PROJECT UAS ALJABAR MATRIX
+Nama : Muhammad Riandra Rai Kurniawan Syah
+
+NPM : 2515061045
+
+KELAS : PSTI B
---
+++ test.py
@@ -0,0 +1,9 @@
+import riandra045 as mt
+
+m1 = [[1, 2], [3, 4]]
+m2 = [[5, 6], [7, 8]]
+
+print(mt.Pertambahanmatrix(m1,m2))
+print(mt.Penguranganmatrix(m1,m2))
+print(mt.Perkalianmatrix(m1,m2))
+print(mt.Pembagianmatrix(m1,m2))
